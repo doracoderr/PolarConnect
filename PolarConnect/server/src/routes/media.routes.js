080afaeadc1efdd789b6a2b35bf51c7bf8f87d08
doracoderr <abhishekprasad9549@gmail.com>
2026-09-23 17:34:36 +0530

    remove same files of code

--- PolarConnect/server/src/routes/media.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { streamMedia } = require("../controllers/media.controller");
-
-const router = express.Router();
-
-router.get("/:id", streamMedia);
-
-module.exports = router;
--- PolarConnect/server/src/routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUploadSignature } = require("../controllers/upload.controller");
-const { requireAuth } = require("../middleware/auth");
-
-const router = express.Router();
-
-router.get("/signature", requireAuth, getUploadSignature);
-
-module.exports = router;
